64742e8a2d1fdbec9890a086c4e35d275721a53b
ej <elijah.cajucom@gmail.com>
2024-07-06 14:55:08 +0800

    initial project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-[10px] text-gray-900'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
